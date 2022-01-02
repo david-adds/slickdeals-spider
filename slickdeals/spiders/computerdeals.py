632a5c94e5248c6807d82be6e0de0285d48a348d
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-02 02:39:21 -0300

    scrape using scrapy-selenium

--- slickdeals/spiders/computerdeals.py
+++ slickdeals/spiders/computerdeals.py
@@ -1,10 +1,33 @@
 import scrapy
+from scrapy_selenium import SeleniumRequest
 
 
 class ComputerdealsSpider(scrapy.Spider):
     name = 'computerdeals'
-    allowed_domains = ['slickdeals.net/computer-deals']
-    start_urls = ['http://slickdeals.net/computer-deals/']
-
+    
+    def start_request(self):
+        yield SeleniumRequest(
+            url="https://slickdeals.net/computer-deals/",
+            wait_time=3,
+            callback = self.parse
+        )
+        
     def parse(self, response):
-        pass
+        products = response.xpath("//ul[@class='dealTiles categoryGridDeals blueprint']/li") #[contains(@class,'fpGridBox grid')]")
+        for product in products:
+            # try:
+            yield{
+                "name": product.xpath(".//a[contains(@class,'itemTitle bp-p-deal')]/text()").get(),
+                "link": product.xpath(".//a[contains(@class,'itemTitle bp-p-deal')]/@href").get(),
+                "store_name": product.xpath(".//button[contains(@class,'itemStore')]/text() | //a[contains(@class,'itemStore bp-p-store')]/text()").get(),
+                "price": product.xpath(".//div[contains(@class,'itemPrice')]/text()").get()
+            }
+        next_page = response.xpath("//a[@data-role='next-page']/@href").get()
+        if next_page:
+            absolute_url = f"https://slickdeals.net{next_page}"
+            yield SeleniumRequest(
+                url=absolute_url,
+                wait_time=3,
+                callback=self.parse
+            )
+                
